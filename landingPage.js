155071c57613b0b4606c0c116f27258dfcda3036
Jaime744 <jaim3cr94@gmail.com>
2025-11-09 13:31:49 +0100

    retoques
    Quite el inicio de sesión y el registro por un botón de únete a nosotros para que vaya directamente al formulario y cambiamos los colores en vez de las líneas que había antes

--- landingPage.js
+++ landingPage.js
@@ -5,3 +5,9 @@ const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+document.getElementById("goToForm").addEventListener("click", function(event) {
+  event.preventDefault();
+  const seccion = document.querySelector(".Form-Section");
+  seccion.scrollIntoView({ behavior: "smooth" });
+});
